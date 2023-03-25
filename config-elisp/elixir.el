4d1d6af3f55453224e50a08872247c84401e4f87
Jason <jasonmj@gmail.com>
2023-03-25 17:25:09 -0400

    Fixup elixir and heex modes configuration

--- config-elisp/elixir.el
+++ config-elisp/elixir.el
@@ -7,32 +7,25 @@
 
 (defvar elixir-outline-regexp
   (concat "^[[:space:]]*\\("
-	  "@moduledoc\\|@behaviour\\|@callback\\|@type\\|@typedoc\\|@doc\\|@spec"
+	  "@moduledoc\\|@behaviour\\|@callback\\|@type\\|@typedoc\\|@doc\\|@spec\\|@impl"
 	  "\\|def\\(\\|p\\|callback\\|delegate\\|impl\\|overridable\\|exception\\|struct\\|guard\\|guardp\\|record\\|recordp\\|macro\\|macrop\\|macrocallback\\|protocol\\)"
 	  "\\|describe\\|test\\|setup\\|let\\|it\\|context\\|before"
 	  "\\|use\\|alias\\|import\\|require"
 	  "\\)\\([[:space:]]\\|(\\)"))
 (defun my/elixir-format ()
   (interactive)
-  (with-current-buffer (current-buffer) 
+  (with-current-buffer (current-buffer)
     (if (f-exists? (concat (project-root (project-current)) "/shell.nix"))
 	(call-process-shell-command (concat "cd " (project-root (project-current)) " && " "NIX_SKIP_SHELL_HOOK=true nix-shell --run \"mix format " (buffer-file-name) "\""))
       (elixir-format)))
   (revert-buffer t t))
 (defun setup-elixir ()
-  (if (f-exists? (concat (project-root (project-current)) "/shell.nix"))
-      (add-hook 'after-save-hook 'my/elixir-format nil 'make-it-local)
-      (add-hook 'before-save-hook 'my/elixir-format nil 'make-it-local))
+  (add-hook 'after-save-hook 'my/elixir-format nil 'make-it-local)
   (setq-local outline-regexp elixir-outline-regexp))
 (use-package elixir-mode :ensure t)
 (use-package elixir-ts-mode
   :ensure t
-  :config
-  (add-hook 'elixir-ts-mode-hook 'setup-elixir)
-  (add-to-list 'auto-mode-alist '("\\.leex\\'" . elixir-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.heex\\'" . elixir-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-ts-mode))
-  (add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-ts-mode)))
+  :hook (elixir-ts-mode . setup-elixir))
 
 (use-package eglot
   :config
@@ -46,19 +39,39 @@
 (use-package polymode
   :ensure t
   :config
-  (define-hostmode poly-elixir-hostmode :mode 'elixir-ts-mode)
-  (define-innermode poly-elixir-doc-innermode
+  (define-hostmode poly-elixir-ts-hostmode :mode 'elixir-ts-mode)
+  (define-innermode poly-elixir-ts-doc-innermode
     :mode 'markdown-mode
     :head-matcher "@\\(module\\)?doc *\"\"\""
     :tail-matcher "\"\"\""
     :head-mode 'host
     :tail-mode 'host)
-  (define-innermode poly-elixir-template-innermode
+  (define-innermode poly-elixir-ts-template-innermode
     :mode 'web-mode
-    :head-matcher "~\\(L\\|E\\)\"\"\""
+    :head-matcher "~\\(L\\|E\\|\\H\\)\"\"\""
     :tail-matcher "\"\"\""
     :head-mode 'host
     :tail-mode 'host)
-  (define-polymode poly-elixir-mode
-    :hostmode 'poly-elixir-hostmode
-    :innermodes '(poly-elixir-doc-innermode poly-elixir-template-innermode)))
+  (define-polymode poly-elixir-ts-mode
+    :hostmode 'poly-elixir-ts-hostmode
+    :innermodes '(poly-elixir-ts-doc-innermode poly-elixir-ts-template-innermode))
+  (define-hostmode poly-heex-ts-hostmode :mode 'heex-ts-mode)
+  (define-innermode poly-heex-ts-doc-innermode
+    :mode 'markdown-mode
+    :head-matcher "@\\(module\\)?doc *\"\"\""
+    :tail-matcher "\"\"\""
+    :head-mode 'host
+    :tail-mode 'host)
+  (define-innermode poly-heex-ts-template-innermode
+    :mode 'web-mode
+    :head-matcher "~\\(L\\|E\\|\\H\\)\"\"\""
+    :tail-matcher "\"\"\""
+    :head-mode 'host
+    :tail-mode 'host)
+  (define-polymode poly-heex-ts-mode
+    :hostmode 'poly-heex-ts-hostmode
+    :innermodes '(poly-heex-ts-doc-innermode poly-heex-ts-template-innermode))
+  (add-to-list 'auto-mode-alist '("\\.leex\\'" . poly-heex-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.heex\\'" . poly-heex-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.ex\\'" . poly-elixir-ts-mode))
+  (add-to-list 'auto-mode-alist '("\\.exs\\'" . poly-elixir-ts-mode)))
