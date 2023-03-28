1fac8803e105caf9dbe322ca143bc7ac1efdf37b
Jason <jasonmj@gmail.com>
2023-03-28 10:37:51 -0400

    Small adjustments for consult and orderless

--- config-elisp/consult.el
+++ config-elisp/consult.el
@@ -21,6 +21,7 @@
          ("M-s D" . consult-locate)
          ("M-s g" . consult-grep)
          ("M-s G" . consult-git-grep)
+         ("s-s" . consult-ripgrep)
          ("M-s r" . consult-ripgrep)
          ("M-s l" . consult-line)
          ("M-s L" . consult-line-multi)
--- config-elisp/orderless.el
+++ config-elisp/orderless.el
@@ -1,6 +1,6 @@
 (use-package orderless
   :ensure t
   :init
-  (setq completion-styles '(orderless basic partial-completion)
+  (setq completion-styles '(orderless basic)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
