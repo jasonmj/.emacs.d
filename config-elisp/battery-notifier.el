c1d495957e38a602ae55c8eae9b1794f76416f02
Jason <jasonmj@gmail.com>
2023-04-06 13:08:41 -0400

    Configure battery-notifier to use ednc

--- config-elisp/battery-notifier.el
+++ config-elisp/battery-notifier.el
@@ -1,4 +1,11 @@
 (use-package battery-notifier
   :ensure t
+  :after (ednc)
+  :config
+  (defun my/battery-notifier-notification-function (message)
+    (notifications-notify :title "Battery Notifier" :body message :app-name "Emacs" :app-icon nil
+		      :actions '("default" "default")))
+  :custom
+  (battery-notifier-notification-function 'my/battery-notifier-notification-function)
   :hook ((after-init . battery-notifier-mode)
 	 (battery-notifier-capacity-critical . (lambda () (call-process-shell-command "systemctl suspend")))))
